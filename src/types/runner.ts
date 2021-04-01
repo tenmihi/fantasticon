b91a6544c43e56ccc43772410eafb1715fe21e73
tenmihi <tenmihi@gmail.com>
2021-04-01 18:18:24 +0900

    Can configure startCodepoint

--- src/types/runner.ts
+++ src/types/runner.ts
@@ -23,6 +23,7 @@ export type RunnerOptionalOptions = {
   templates: { [key in OtherAssetType]?: string };
   prefix: string;
   fontsUrl: string;
+  startCodepoint: number;
 };
 
 export type RunnerOptionsInput = RunnerMandatoryOptions &
